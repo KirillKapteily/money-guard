727e00b8610812d3e52aa6105d2ddc141a1cbd9c
Кирилл Каптелий <161931268+KirillKapteily@users.noreply.github.com>
2026-06-20 20:13:48 +0300

    v 1.3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,24 @@
-export default function Header({}) {
+//header.module
+import styles from "../styles/header.module.scss"
 
+export default function Header({name  ,  loggingout}) {
+    return (
+        <header>
+            <div>
+                <svg className={styles.logo}><use href="./symbol-defs-k-header.svg#icon-money-guard-merged-logo" width="115" height="48"></use></svg>
+            </div>
+
+            <div className={styles.controls__wrapper}>
+                <div>
+                    <p className={styles.name__display}>{name}</p>
+                </div>
+                <hr className={styles.hr}/>
+                <div className={styles.exit__wrapper} onClick={()=> loggingout(true)}>
+                    <p className={styles.exit__text}> <svg className={styles.exit}><use href="./symbol-defs-k-header.svg#icon-exit" width="18" height="18"></use></svg>Exit</p>
+                </div>
+            </div>
+
+
+        </header>
+    )
 }
