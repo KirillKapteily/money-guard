3a2319d06d84a3cf8b8296b1a65cc7be79dea88f
llarq <s.leushkin1509@gmail.com>
2026-06-24 14:11:42 +0300

    add first ver of home screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
+import HomeSidebar from "./HomeSidebar";
 import Sidebar from "./Sidebar";
 import TransactionsList from "./TransactionsList";
 
 export default function Home({ transactions, setTransactions }) {
   return (
     <>
+      <HomeSidebar transactions={transactions}/>
       <Sidebar />
       <TransactionsList 
         transactions={transactions} 
